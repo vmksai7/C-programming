5aa8a5400e277542ef8db307ed6d650d0a871922
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-04 13:39:59 +0530

    Binaryequivalent.c
    C program to print binary equivalent of given data

---
+++ binaryequivalent.c
@@ -0,0 +1,17 @@
+//C program to print binary equivalent of double using char pointer
+
+#include<stdio.h>
+int main()
+{
+  double d;
+  int i,j;
+  printf("Enter the data\n");
+  scanf("%lf",&d); //Reading data from user using %lf(double) value
+  char *p=&d,cp;
+  for(i=7;i>=0;i--)
+  {
+    cp=*(p+i);
+    for(j=7;j>=0;j--)
+      printf("%d",(cp>>j)&1);
+  }
+}
