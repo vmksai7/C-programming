8bf9a7f5ccc3e20a35e0216fa97310adaa2e7641
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-04 13:12:12 +0530

    helloworld.c
    C program to print hello world

---
+++ helloworld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>//It's a preprocessor which includes function definations like printf() and scanf()
+
+int main()
+{
+  printf("Hello World\n");//It's a printf statement to print the output and ";" should be mandatory at the end of every statement
+}
