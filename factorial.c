8e4bcbb16dd1dd0000e62b1c500eafcbede4a0ce
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-05 09:38:51 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    unsigned long long factorial = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // If the user enters a negative number, display an error message.
+    if (number < 0) {
+        printf("Error! Factorial is not defined for negative numbers.\n");
+    } else {
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        printf("Factorial of %d = %llu\n", number, factorial);
+    }
+
+    return 0;
+}
