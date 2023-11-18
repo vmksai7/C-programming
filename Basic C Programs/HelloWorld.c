a9ff19752bf2c4f94c6f146c23acc07213ae07f7
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-18 09:59:03 +0530

    HelloWorld.c

--- Basic C Programs/HelloWorld.c
+++ Basic C Programs/HelloWorld.c
@@ -1,7 +1,7 @@
 //C Program to print Hello World
 
 /*#include is a precessor directive and <stdio.h> is standard I/O library includes
-declarations of functions like printf, scanf, fopen, fclose, getchar, putchar*/
+declarations of functions like printf, scanf, fopen, fclose, getchar, putchar...*/
   
 #include<stdio.h>
 int main()
