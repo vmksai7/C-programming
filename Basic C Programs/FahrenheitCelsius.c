050f95c4453442ca450e22ff1016126535ae7730
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-18 12:22:11 +0530

    FahrenheitCelsius.c

--- Basic C Programs/FahrenheitCelsius.c
+++ Basic C Programs/FahrenheitCelsius.c
@@ -24,7 +24,7 @@ main()
 
 #include<Stdio.h>
 
-////Print Fahrenheit Celsius table for fahr=0,20,....300 in floating point version
+//Print Fahrenheit Celsius table for fahr=0,20,....300 in floating point version
 
 main()
 {
@@ -40,4 +40,22 @@ main()
     printf("%3.0f\t %6.1f\n",fahr,cels);
     fahr=fahr+step;
   }
+}
+
+//Same program by using for loop
+
+#include<Stdio.h>
+main()
+{
+  for(float fahr=0;fahr<=300;fahr+=20)
+    printf("%3.0f %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+}
+
+//Same program by using for loop in reverse order
+
+#include<Stdio.h>
+main()
+{
+  for(float fahr=300;fahr>=0;fahr-=20)
+    printf("%3.0f %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
 }
