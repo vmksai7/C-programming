4304419c8a411fdd274b285702f358cff792cbcc
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-24 16:52:17 +0530

    Relational.c

---
+++ Basic C Programs/Operators & Expression/Relational.c
@@ -0,0 +1,17 @@
+#include<Stdio.h>
+int main()
+{
+  int a=5,b=-2,c;
+  c=a>b;
+  printf("%d\n",c); //1 (True)
+  c=a<b;
+  printf("%d\n",c); //0 (False)
+  c=a>=b;
+  printf("%d\n",c); //1
+  c=a<=b;
+  printf("%d\n",c); //0
+  c=a==b;
+  printf("%d\n",c); //0
+  c=a!=b;
+  printf("%d\n",c); //1
+}
