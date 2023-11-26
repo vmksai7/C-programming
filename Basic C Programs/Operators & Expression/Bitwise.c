ffa0ca34c4da8d99869164d484cb10a31ae06e46
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-26 17:53:14 +0530

    Bitwise.c

--- Basic C Programs/Operators & Expression/Bitwise.c
+++ Basic C Programs/Operators & Expression/Bitwise.c
@@ -9,3 +9,19 @@ int main()
   a = a ^ b;
   printf("%d\n",c); //30
 }
+
+#include<stdio.h>
+int main()
+{
+  int a=10,b=20,c;
+  c = a>>1;
+  printf("%d\n",c); //5
+  c = a<<1;
+  printf("%d\n",c); //20
+  c = a<<1 & a>>1;
+  printf("%d\n",c); //4
+  c = a<<1 | a>>1;
+  printf("%d\n",c); //21
+  c = ~(a<<1 | a>>1);
+  printf("%d\n",c); //-22
+}
