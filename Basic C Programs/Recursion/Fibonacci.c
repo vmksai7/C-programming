617d9352e070e1bf9489f0d0e10f64dff0e5a769
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-27 18:33:29 +0530

    Fibonacci.c

---
+++ Basic C Programs/Recursion/Fibonacci.c
@@ -0,0 +1,21 @@
+//C program to generate fibonacci series using recustion
+
+#include<Stdio.h>
+int fibonacci(int n)
+{
+  if(n<=1)
+    return n;
+  else
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main()
+{
+  int terms,i;
+  printf("Enter number of terms in the fibonacci series\n");
+  scanf("%d",&terms);
+  printf("Fibonacci series\n");
+  for(i=0;i<terms;i++)
+    printf("%d ",fibonacci(i));
+  return 0;
+}
