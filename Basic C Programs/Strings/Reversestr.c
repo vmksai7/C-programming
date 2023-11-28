27840cfb4701689963e118801adda973f8cb2223
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-28 10:54:33 +0530

    Reversestr.c

---
+++ Basic C Programs/Strings/Reversestr.c
@@ -0,0 +1,17 @@
+#include<Stdio.h>
+#include<string.h>
+int main()
+{
+  char str[50];
+  int i,cnt,temp;
+  scanf("%s",str);
+  for(i=0;str[i];i++);
+  cnt=i;
+  for(i=0;i<cnt/2;i++)
+  {
+    temp=str[i];
+    str[i]=str[cnt-i-1];
+    str[cnt-i-1]=temp;
+  }
+  printf("%s",str);
+}
