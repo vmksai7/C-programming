dd51b4eaad806432f1f5caa634f4b623abf5d1e0
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-28 10:58:42 +0530

    secondsmallestno.c

---
+++ Basic C Programs/Arrays/secondsmallestno.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define N 5
+
+int main() {
+    int arr[N];
+    int i,min1,min2,temp;
+    printf("Enter %d integers\n",N);
+    for(i=0;i<N;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<N;i++)
+        printf("%d ",arr[i]);
+    printf("\n");
+    min1=arr[0];
+    min2=arr[1];
+    if(min2<min1)
+    {
+        temp=min2;
+        min2=min1;
+        min1=temp;
+    }
+    for(i=2;i<N;i++)
+    {
+        if(arr[i]<min1)
+        {
+            min2=min1;
+            min1=arr[i];
+        }
+        else if(arr[i]<min2)
+            min2=arr[i];
+    }
+    printf("Second smallest number in an array is: %d\n",min2);
+}
