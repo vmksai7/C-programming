57215122deb3d353699dfe128ab4460699a088ff
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-28 11:00:48 +0530

    Removeduplicates.c

---
+++ Basic C Programs/Arrays/Remoceduplicates.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define N 10
+int main()
+{
+  int arr[N],i,j,k,num;
+  printf("Enter %d elements:\n",N);
+  for(i=0;i<N;i++)
+    scanf("%d",&arr[i]);
+  for(i=0;i<N;i++)
+    printf("%d ",arr[i]);
+  printf("\n");
+  num=N;
+  for(i=0;i<num;i++)
+  {
+    for(j=i+1;j<num;)
+    {
+      if(arr[i]==arr[j])
+      {
+        for(k=j;k<num;k++)
+          arr[k]=arr[k+1];
+        num--;
+      }
+      else
+        j++;
+    }
+  }
+  for(i=0;i<num;i++)
+    printf("%d ",arr[i]);
+}
