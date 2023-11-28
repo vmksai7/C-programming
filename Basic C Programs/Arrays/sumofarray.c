dc55ce3a42fadc219aa63aeaf3f0f641b91045a3
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-11-28 10:57:58 +0530

    sumofarray.c

---
+++ Basic C Programs/Arrays/sumofarray.c
@@ -0,0 +1,17 @@
+//C program to add the digits of an array
+#include<stdio.h>
+#define N 5
+int main()
+{
+  int arr[N];
+  int i,n,sum=0;
+  printf("Enter %d elements\n",N);
+  for(i=0;i<N;i++)
+    scanf("%d",&arr[i]);
+  for(i=0;i<N;i++)
+    printf("%d ",arr[i]);
+  printf("\n");
+  for(i=0;i<N;i++)
+    sum=sum+arr[i];
+  printf("Sum of an array is: %d",sum);
+}
