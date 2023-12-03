e3741f69dff3c9ec93f52b4a98d0a353f6839700
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-03 10:04:43 +0530

    clearbit.c

---
+++ Basic C Programs/Bit Manipulation/clearbit.c
@@ -0,0 +1,14 @@
+//C program to clear a bit position of a given number
+
+#include<stdio.h>
+int main()
+{
+    int num,bit,mask,res;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    printf("Enter the position bit is  to be set\n");
+    scanf("%d",&bit);
+    mask=~(1<<(bit-1));
+    res = num & mask;
+        printf("Result is: %d\n", res);
+}
