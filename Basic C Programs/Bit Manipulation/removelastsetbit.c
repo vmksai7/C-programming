da8d23551ad853677ab646ff3619f7af9fb627f6
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-03 10:33:23 +0530

    removelastsetbit.c

--- Basic C Programs/Bit Manipulation/removelastsetbit.c
+++ Basic C Programs/Bit Manipulation/removelastsetbit.c
@@ -9,3 +9,18 @@ int main()
     res = num & (num-1);
         printf("Result is: %d\n", res);
 }
+
+//C program to find the rightmost set bit position
+
+#include<stdio.h>
+int main()
+{
+    int num,bit,mask,res;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    //printf("Enter the position bit is  to be set\n");
+    //scanf("%d",&bit);
+    //mask=~(1<<(bit-1));
+    res = num & ~(num-1);
+        printf("Result is: %d\n", res);
+}
