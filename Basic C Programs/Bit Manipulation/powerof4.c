591d6517d1d4c748f9369c09a84a90bf8db97d4b
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-04 16:07:45 +0530

    powerof4.c

---
+++ Basic C Programs/Bit Manipulation/powerof4.c
@@ -0,0 +1,27 @@
+//C program to check if given number is power of 8 or not
+
+#include<stdio.h>
+
+int power(int num)
+{
+    if(num==0)
+        return 0;
+    while(num!=1)
+    {
+        if(num%8!=0)
+            return 0;
+        num=num/8;
+    }
+    return 1;
+}
+
+int main()
+{
+    int num,temp;
+    printf("Enter the number to check if it power of 8\n");
+    scanf("%d",&num);
+    if(power(num))
+        printf("%d is power of 8\n",num);
+    else
+        printf("%d is not power of 8\n",num);
+}
