c5ee6a55e80b329f8098ebc06b65c4b0afea642d
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-04 16:17:10 +0530

    toggleithdigit.c

---
+++ Basic C Programs/Bit Manipulation/toggleithdigit.c
@@ -0,0 +1,20 @@
+//C program to toggle ith digit of a given number
+
+#include<stdio.h>
+
+int Toggle(int num,int bit)
+{
+    int temp=1<<(bit-1);
+    return (num^temp);
+}
+
+int main()
+{
+    int num,bit,res;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    printf("Enter the bit position that need to toggle\n");
+    scanf("%d",&bit);
+    res=Toggle(num,bit);
+    printf("%d\n",res);
+}
