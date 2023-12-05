98980768789f3488d62701b2e99aae60da697aed
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-05 11:46:25 +0530

    findletterpos.c

---
+++ Basic C Programs/Bit Manipulation/findletterpos.c
@@ -0,0 +1,12 @@
+//C program to find the letter position of the alphabet using bitwise operator
+
+#include<stdio.h>
+int main()
+{
+  char letter;
+  int pos;
+  printf("Enter the letter\n");
+  scanf("%c",&letter);
+  pos=letter&0x1F;
+  printf("Position of %c is %d\n",letter,pos);
+}
