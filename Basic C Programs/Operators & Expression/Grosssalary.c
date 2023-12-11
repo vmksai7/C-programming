6ec0b7344c74672c94101e316a3888981d01470d
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-11 13:58:10 +0530

    Grosssalary.c

---
+++ Basic C Programs/Operators & Expression/Grosssalary.c
@@ -0,0 +1,16 @@
+//Calculate Employee's Gross salary
+#include<stdio.h>
+int main()
+{
+  float sal,da,hra,grpay;
+  printf("Enter Employee's basic salary\n");
+  scanf("%f",&sal);
+  da=0.4*sal;
+  hra=0.2*sal;
+  grpay=sal+da+hra;
+  printf("Basic salary of Employee = %f\n",sal);
+  printf("Dearness Allowances = %f\n",da);
+  printf("House rent Allowances = %f\n",hra);
+  printf("Gross salary = %f\n",grpay);
+  return 0;
+}
