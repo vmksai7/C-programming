a7f4798fd9e369f86c47ac260c81661711a76f97
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-11 13:47:52 +0530

    simpleinterest.c

---
+++ Basic C Programs/simpleinterest.c
@@ -0,0 +1,14 @@
+//Calculation on simple Interest
+#include<stdio.h>
+int main()
+{
+  int princ,noy;
+  float roi,si;
+  princ=1000;
+  noy=3;
+  roi=8.5;
+  //Formula for simple Interest
+  si=princ*noy*roi/100;
+  printf("%f\n",si);
+  return 0;
+}
