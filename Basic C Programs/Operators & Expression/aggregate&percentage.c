82ca39af1e01fad148f5f942d9341b34fa5bc11b
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-11 15:54:08 +0530

    aggregate&percentage.c

---
+++ Basic C Programs/Operators & Expression/aggregate&percentage.c
@@ -0,0 +1,14 @@
+//C program to find the aggregate and percentage of a student of 5 subjects marks
+
+#include<stdio.h>
+int main()
+{
+  int m1,m2,m3,m4,m5,aggr;
+  float perc;
+  printf("Enter marks in 5 subjects\n");
+  scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
+  aggr=m1+m2+m3+m4+m5;
+  perc=aggr/5;
+  printf("Aggregate Marks = %d\n",aggr);
+  printf("Percentage marks = %f\n",perc);
+}
