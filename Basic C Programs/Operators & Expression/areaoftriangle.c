e3c7446bf159040f8b7aed35295acb94d0d90b0f
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-12 11:54:33 +0530

    areaoftriangle.c

---
+++ Basic C Programs/Operators & Expression/areaoftriangle.c
@@ -0,0 +1,15 @@
+//C program to find the area of triangle, given its sides
+
+#include<stdio.h>
+#include<math.h> //To use sqrt()
+
+int main()
+{
+  float a,b,c,sp,area;
+  printf("Enter the sides of a triangle\n");
+  scanf("%f %f %f",&a,&b,&c);
+  sp=(a+b+c)/2;
+  area=sqrt(sp*(sp-a)*(sp-b)*(sp-c));
+  printf("Area of triangle= %f\n",area);
+  return 0;
+}
