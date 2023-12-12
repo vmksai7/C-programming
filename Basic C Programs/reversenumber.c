236c0b976d5afc6a8cc4f5c59217169300e852bc
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-12 12:00:39 +0530

    reversenumber.c

---
+++ Basic C Programs/reversenumber.c
@@ -0,0 +1,16 @@
+//C program to reverse the given number
+
+#include<stdio.h>
+int main()
+{
+  int num,temp,res=0;
+  printf("Enter the number =");
+  scanf("%d",&num);
+  while(num!=0)
+  {
+    temp=num%10;
+    res=res*10+temp;
+    num=num/10;
+  }
+  printf("Reversed number is = %d\n",res);
+}
