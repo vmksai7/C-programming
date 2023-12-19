f1f19114acb64c6c6f013b779fea02c621d7ee7b
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-19 16:26:01 +0530

    evenodd.c

---
+++ Basic C Programs/Conditional Operator/evenodd.c
@@ -0,0 +1,16 @@
+//C program to check if given number is even or odd usinf conditional operator
+
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    // Input
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // Determine even or odd using the conditional operator
+    (number % 2 == 0) ? printf("%d is even.\n", number) : printf("%d is odd.\n", number);
+
+    return 0;
+}
