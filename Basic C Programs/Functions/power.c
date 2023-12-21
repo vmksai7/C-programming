d276ffbd7c81300280ba89ba652ff01f21a68851
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-21 15:34:48 +0530

    power.c

---
+++ Basic C Programs/Functions/power.c
@@ -0,0 +1,22 @@
+//C program to calculate the value of a raised to b using function power(a,b)
+
+#include<stdio.h>
+float power(float,int);
+int main()
+{
+  float x,pow;
+  int y;
+  printf("Enter two numbers\n");
+  scanf("%f %d",&x,&y);
+  pow=power(x,y);
+  printf("%f to the power of %d = %f\n",x,y,pow);
+  return 0;
+}
+float power(float x, int y)
+{
+  int i;
+  float p=1;
+  for(i=1;i<=y;i++)
+    p=p*x;
+  return p;
+}
