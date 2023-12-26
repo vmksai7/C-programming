c728fd50880ecba8bdaec6a73844fa192c64643b
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-26 17:56:55 +0530

    minimumof2nums.c

---
+++ Basic C Programs/Conditional Operator/minimumof2nums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables to store two numbers
+    double num1, num2;
+
+    // Input the two numbers
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    // Find the minimum using the conditional operator
+    double min = (num1 < num2) ? num1 : num2;
+
+    // Display the result
+    printf("The minimum of %.2lf and %.2lf is %.2lf\n", num1, num2, min);
+
+    return 0;
+}
