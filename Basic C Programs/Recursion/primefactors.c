abca7ec22cdd70d6e0d9a6fe930813148d0397f7
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2023-12-29 16:59:04 +0530

    primefactors.c

---
+++ Basic C Programs/Recursion/primefactors.c
@@ -0,0 +1,27 @@
+//C program to find the prime factors of a given number
+
+#include<stdio.h>
+void factorize(int ,int);
+int main()
+{
+  int num;
+  printf("Enter number: ");
+  scanf("%d",&num);
+  printf("Prime factors are: ");
+  factorize(num,2);
+  return 0;
+}
+void factorize(int n,int i)
+{
+  if(i<=n)
+  {
+    if(n % i == 0)
+    {
+      printf("%d",i);
+      n = n / i;
+    }
+    else
+      i++;
+    factorize(n,i);
+  }
+}
