e587d871d8823a5342d656b256a6fb79aa8df5b1
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2024-01-02 11:07:11 +0530

    Reversearr.c

---
+++ Basic C Programs/Arrays/Reversearr.c
@@ -0,0 +1,18 @@
+//C program to copy 1 array into another in reverse order
+
+#include<stdio.h>
+#define N 5
+
+int main()
+{
+    int arr1[N],arr2[N],i,j;
+    printf("Enter %d elements of an array\n",N);
+    for(i=0;i<=4;i++)
+        scanf("%d",&arr1[i]);
+    for(i=0,j=4;i<=4;i++,j--)
+        arr2[j]=arr1[i];
+    printf("Elements in reverse order\n");
+    for(i=0;i<=4;i++)
+        printf("%d ",arr2[i]);
+    return 0;
+}
