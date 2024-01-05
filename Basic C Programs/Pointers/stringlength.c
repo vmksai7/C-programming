ee4105d8193cdb4396cc3c0cd3cb89272a5e807a
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2024-01-05 12:38:40 +0530

    stringlength.c

---
+++ Basic C Programs/Pointers/stringlength.c
@@ -0,0 +1,18 @@
+//C program to find the string length using pointers
+
+#include<stdio.h>
+int length(const char *str)
+{
+    const char *ptr = str;
+    while(*ptr!='\0')
+    {
+        ptr++;
+    }
+    return ptr-str;
+}
+int main()
+{
+    const char *str = "Manikanta Sai";
+    int len = length(str);
+    printf("Length of string is %d\n",len);
+}
