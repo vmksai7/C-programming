e6518db36ee12b80cb1da42d1e2ce78521f37f0a
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2024-01-07 16:06:35 +0530

    Arraychecknumber.c

--- Basic C Programs/Arrays/Arraychecknumber.c
+++ Basic C Programs/Arrays/Arraychecknumber.c
@@ -1,30 +1,31 @@
+/*******************************************************************************
+C Program to check the given number is present in an array or not
+******************************************************************************/
+
 #include<stdio.h>
-#define N 10
+
+#define N 5
 
 int main()
 {
-  int arr1[N],i,m,found;
-  for(i=0;i<N;i++)
-  {
-      printf("Enter the %d element: ",i);
-      scanf("%d",&arr1[i]);
-  }
-  printf("\n");
-  for(i=0;i<N;i++)
-    printf("%d ",arr1[i]);
-  printf("\n");
-  printf("Enter the number to be searched in an array: ");
-  scanf("%d",&m);
-  found=0;
-  for(i=0;i<N;i++)
-  {
-      if(arr1[i]==m)
-      {
-          found=1;
-      }
-  }
-  if(found==1)
-    printf("Number found\n");
-  else
-    printf("Number not found\n");
+    int arr1[N];
+    int num,num1,found=0;
+
+    printf("Enter %d elements of 1st array\n",N);
+    for(num=0;num<N;num++)
+        scanf("%d",&arr1[num]);
+    for(num=0;num<N;num++)
+        printf("%d ",arr1[num]);
+
+    printf("\nEnter the number to be searched in an array\n");
+    scanf("%d",&num1);
+    for(num=0;num<N;num++)
+    {
+        if(arr1[num]==num1)
+            found=1;
+    }
+    if(found==1)
+        printf("%d is found in an array\n",num1);
+    else
+        printf("%d is not found in an array\n",num1);
 }
