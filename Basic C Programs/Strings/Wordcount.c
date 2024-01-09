ec89ef8d405e50825e2ebd6d8b29ad0936972304
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2024-01-09 16:01:18 +0530

    Wordcount.c

---
+++ Basic C Programs/Strings/Wordcount.c
@@ -0,0 +1,26 @@
+/*******************************************************************************
+C program to count the number of words in a string
+*******************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100];
+    int word=0,i;
+
+    printf("Enter a string\n");
+    gets(str);
+
+    //Finding the length of a string
+    int len=strlen(str);
+    for(i=0;i<len;i++)
+    {
+        if((str[i]==' ') || (str[i]=='\n') || (str[i]=='\t'))
+        {
+            word++;
+        }
+    }
+    printf("Words in a given string is %d\n",word+1);
+}
