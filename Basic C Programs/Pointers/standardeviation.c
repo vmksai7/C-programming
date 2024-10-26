8ea49375b85ff5c13f0c07381cf6bae9835b64f6
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2024-10-26 19:50:57 +0530

    standardeviation.c

--- Basic C Programs/Pointers/standardeviation.c
+++ Basic C Programs/Pointers/standardeviation.c
@@ -22,3 +22,4 @@ void stats(int *sum, int *avg, double *stdev)
   *avg=*sum/5;
   *stdev=sqrt((pow((n1-*avg),2.0)+pow((n2-*avg),2.0)+pow((n3-*avg),2.0)+pow((n4-*avg),2.0)+pow((n5-*avg),2.0))/4);
 }
+
