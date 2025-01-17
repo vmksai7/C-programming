c3665912298c6db9163f3be713e4b25ddfd75e88
Manikanta Sai <75871719+vmksai7@users.noreply.github.com>
2025-01-18 04:46:00 +0530

    Evenodd.c

--- Basic C Programs/Bit Manipulation/evenodd.c
+++ Basic C Programs/Bit Manipulation/evenodd.c
@@ -12,3 +12,27 @@ int main()
     else
         printf("Number is even\n");
 }
+
+/*******************************************************************************
+C program to find given number is even or odd using Macro
+*******************************************************************************/
+#include<stdio.h>
+
+#define OE(num) (num&1)
+//#define OE(num) ((num&1) ? 1 : 0)
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    
+    if(OE(num))
+    {
+        printf("Odd\n");
+    }
+    else
+    {
+        printf("Even\n");
+    }
+
+}
